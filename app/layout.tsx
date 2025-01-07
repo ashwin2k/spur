e85dd4a72c101bfb37e12732a45ff5ec292c654b
Ashwin Kumar <ashwin102000@gmail.com>
2025-01-07 15:37:10 -0500

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
-import Link from "next/link";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -11,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Ashwin Kumar's Demo",
+  description: "Hire Him!",
 };
 
 const geistSans = Geist({
